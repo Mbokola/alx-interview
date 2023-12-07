f642b176100b9385fe766e1993089079697c3216
Mbokola <tonypeaks@gmail.com>
2023-12-07 18:28:09 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" returns the string representation of the float. """
+
+
+def to_str(n: float) -> str:
+    """ returns the string representation of the float. """
+    return str(n)
