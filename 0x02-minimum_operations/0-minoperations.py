a862f2b8fdde5417fa897811d945cacf680426e1
Mbokola <tonypeaks@gmail.com>
2023-12-12 15:23:17 +0000

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 0-minoperations """
+
+
+def minOperations(n):
+    """
+    Calculates minimum operations required to copy text from
+    textfile n times
+    """
+    if n <= 0:
+        return 0
+
+    copy_and_paste = 2
+    paste = 1
+    clipboard = 0
+    text_count = 1
+    operations = 0
+
+    while n != text_count:
+        if text_count + text_count <= n and text_count != copy_and_paste:
+            operations += copy_and_paste
+            clipboard = text_count
+            text_count += text_count
+        else:
+            operations += paste
+            text_count += clipboard
+    return operations
