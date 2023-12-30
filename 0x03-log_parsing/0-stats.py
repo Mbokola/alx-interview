172ed0035233e7a7a3c02c55c4d31ce1c8faace1
Mbokola <tonypeaks@gmail.com>
2023-12-30 11:43:18 +0000

    Redo 2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,11 +20,12 @@ try:
         except ValueError:
             pass
         status_codes.append(status_code)
-    if count % 10 == 0:
-        print(f"File size: {total_file_size}")
-        for code in ["200", "301", "400", "401", "403", "404", "405", "500"]:
-            if status_codes.count(code):
-                print(f"{code}: {status_codes.count(code)}")
+        if count % 10 == 0:
+            print(f"File size: {total_file_size}")
+            for code in ["200", "301", "400", "401", "403", "404", "405",
+                         "500"]:
+                if status_codes.count(code):
+                    print(f"{code}: {status_codes.count(code)}")
 
 except KeyboardInterrupt:
     pass
