41bacef94528e9028e296baef6f84e14d054b63b
Mbokola <tonypeaks@gmail.com>
2024-01-03 13:34:43 +0000

    Revert

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,22 +4,10 @@
 
 def validUTF8(data):
     """ Checks if encoding is utf8 """
-    count = 0
     for encoding in data:
+        if encoding < 128:
+            continue
         binary = format(encoding, '08b')
-        if count == 0:
-            if binary.startswith('0'):
-                continue
-            elif binary.startswith('110'):
-                count = 1
-            elif binary.startswith('1110'):
-                count = 2
-            elif binary.startswith('11110'):
-                count = 3
-            else:
-                return False
-        else:
-            if not binary.startswith('10'):
-                return False
-            count -= 1
-    return count == 0
+        if binary.startswith("10"):
+            return False
+    return True
