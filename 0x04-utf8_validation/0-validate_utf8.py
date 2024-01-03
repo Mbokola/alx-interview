7c4119bd604bb9b032013e2d0cb4aa170fd3b63a
Mbokola <tonypeaks@gmail.com>
2024-01-03 14:34:05 +0000

    Try this

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,19 +4,8 @@
 
 def validUTF8(data):
     """ Checks if encoding is utf8 """
-    count = 0
     for encoding in data:
-        binary = format(encoding, '08b')
-        if count == 0:
-            if binary.startswith('0'):
-                continue
-            elif binary.startswith('110'):
-                continue
-            elif binary.startswith('1110'):
-                continue
-            elif binary.startswith('11110'):
-                continue
-            else:
-                return False
-
+        if encoding < 128:
+            continue
+        return False
     return True
