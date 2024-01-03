8552f8ec52404849b1b54d87c17be18dcb0b4bce
Mbokola <tonypeaks@gmail.com>
2024-01-03 13:08:09 +0000

    0. UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 0-validate_utf8 """
+
+
+def validUTF8(data):
+    """ Checks if encoding is utf8 """
+    for encoding in data:
+        if encoding < 128:
+            continue
+        binary = bin(encoding).split("0b")[1]
+        if binary.startswith("11"):
+            continue
+        return False
+    return True
