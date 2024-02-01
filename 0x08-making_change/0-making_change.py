709733b6674540f54353301c7df011a378448dc3
Mbokola <tonypeaks@gmail.com>
2024-02-01 13:07:08 +0000

    Final

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,16 +9,13 @@ def makeChange(coins, total):
     change_coin_count = 0
     coins.sort(reverse=True)
 
-    while (total > 0):
+    while (total > 0 and coins):
         for coin in coins:
             if coin <= total:
                 count = total // coin
                 total -= coin * count
                 change_coin_count += count
-        if not coins:
-            return -1
-
         if total:
             del coins[0]
 
-    return change_coin_count
+    return change_coin_count if not total else -1
