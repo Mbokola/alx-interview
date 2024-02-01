2d6063edbe3764ee5abd05e6f3bee9a039ef9a43
Mbokola <tonypeaks@gmail.com>
2024-02-01 12:05:52 +0000

    More condition

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,11 +10,24 @@ def makeChange(coins, total):
         return 0
 
     change_coin_count = 0
+    store_value = 0
     coins.sort(reverse=True)
+
+    for coin in coins:
+        if not total % coin:
+            store_value = total // coin
+            break
 
     for coin in coins:
         if coin < total:
             count = total // coin
             total -= coin * count
             change_coin_count += count
-    return change_coin_count if not total else -1
+
+    if total and store_value:
+        return store_value
+    if not total and change_coin_count > store_value:
+        return store_value
+    if total and not store_value:
+        return -1
+    return change_coin_count
